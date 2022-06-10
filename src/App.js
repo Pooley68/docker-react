ec87136b8044fb594c7d28f498b01a4a2991c56b
Pooley68 <phil_poole24@hotmail.com>
2022-06-10 19:01:27 +0100

    changed app text 3

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>fuck!</p>
+        <p>fuck oof!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
